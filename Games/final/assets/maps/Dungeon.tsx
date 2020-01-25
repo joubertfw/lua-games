2d2d2e188541e068e835038e3d13f41ba06fbf06
Joubert Filho <joubert.fw@gmail.com>
2020-01-25 16:50:33 -0300

    Added Map 1

--- Games/final/assets/maps/Dungeon.tsx
+++ Games/final/assets/maps/Dungeon.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="Dungeon" tilewidth="64" tileheight="64" tilecount="322" columns="23">
- <image source="../useful/dungeon/Social/Assets.png" width="1472" height="896"/>
+ <image source="Tileset.png" width="1472" height="896"/>
 </tileset>
